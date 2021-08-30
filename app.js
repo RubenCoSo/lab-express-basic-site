bbbfde21459b3f6eb7702bb7cd874ab9919e43e3
Ruben Corral <rubencorralsola@gmail.com>
2021-08-30 23:59:43 +0200

    lab completed

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require("express");
+const express = require(`express`);
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(`public`));
 
-app.get("/home", (request, response, next) => {
-  response.sendFile(__dirname + "/views/home.html");
+app.get(`/`, (request, response, next) => {
+  response.sendFile(__dirname + `/views/home.html`);
 });
 
-app.listen(3500, () => console.log("My first app listening on port 3500! "));
+app.get(`/about`, (request, response, next) => {
+  response.sendFile(__dirname + `/views/about.html`);
+});
+
+app.get(`/works`, (request, response, next) => {
+  response.sendFile(__dirname + `/views/works.html`);
+});
+
+app.get(`/photo-gallery`, (request, response, next) => {
+  response.sendFile(__dirname + `/views/photo-gallery.html`);
+});
+
+app.listen(3500, () => console.log(`My first app listening on port 3500! `));
